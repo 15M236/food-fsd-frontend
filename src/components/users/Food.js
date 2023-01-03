feb186a538078e0747be925526b846fc861f5df2
raghav8197 <raghav8197@gmail.com>
2023-01-03 19:29:16 +0530

    after resolving errors

--- src/components/users/Food.js
+++ src/components/users/Food.js
@@ -1,7 +1,5 @@
 import React, { useEffect,useState,useContext } from 'react'
-import TopBar from './TopBar'
 import Button from 'react-bootstrap/Button';
-// import Table from 'react-bootstrap/Table';
 import axios from 'axios'
 import env from '../../enviroinment'
 import {useNavigate} from 'react-router-dom'
