e6cda1d6d4d443f2baa5cb6ffc0b1ffee25f5d65
raghav8197 <raghav8197@gmail.com>
2023-01-09 01:17:20 +0530

    final commit after changed /

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
               <Route path='/login' element={<Login/>}/>
               <Route path='/signup' element={<Signup/>}/>
               <Route path='/order-success' element={<Success/>}/>
-              <Route path='*' element={<Navigate to='/signup'/>}/>
+              <Route path='*' element={<Navigate to='/login'/>}/>
             </Routes>
         </BrowserRouter>
       </CartContext.Provider>
